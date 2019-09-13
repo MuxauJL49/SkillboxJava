8400c6ac186b2dc6fc09c387bf1a9b56d32e0b75
Mikhail <MuxauJL2006@gmail.com>
2019-09-13 19:43:06 +0600

    task 4.5

--- 04_Unit/5_Task/FIO/src/Loader.java
+++ 04_Unit/5_Task/FIO/src/Loader.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Loader {
     public static void main(String[] args) {
@@ -7,27 +9,24 @@ public class Loader {
         String[] inputTextArr;
 
         System.out.println("Введите (Фамилию Имя Отчество):");
-        String inputText = scaner.nextLine();
+        String inputText = scaner.nextLine().trim();
 
 
-        if (inputText.contains(" ")) {
-            inputTextArr = inputText.split(" +");
-        } else {
-            inputTextArr = new String[]{"", inputText, ""};
-        }
-
-        for (int i = 0; i <= 2; i++) {
-            if(inputTextArr.length - 1 < i) {
+        Matcher matcher = Pattern.compile("\\w+").matcher(inputText);
+        for(int i = 0; matcher.find(); i++){
+            if(i == FIO.length){
+                break;
+            }else if(inputText.contains(" ")){
+                FIO[i][1] = matcher.group();
+            }else{
+                FIO[1][1] = matcher.group();
                 break;
             }
-            FIO[i][1] = inputTextArr[i];
         }
-
         printFIO(FIO);
     }
 
-
-    public static void printFIO(String[][] FIO) {
+    private static void printFIO(String[][] FIO) {
         for (String[] item : FIO) {
             if (!item[1].equals("")) {
                 System.out.println(item[0] + " - " + item[1]);
